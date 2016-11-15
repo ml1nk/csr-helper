b64e70bad54ea873145acf5e3056f356efb25ad7
Marcel Link <marcel.link@t-online.de>
2016-11-15 20:04:25 +0100

    // work in progress

--- csr/ServerPass_Standard.js
+++ csr/ServerPass_Standard.js
@@ -1,40 +1,20 @@
-/*
-{
-  C : "C"
-  O : "O",
-  OU1 : "OU",
-  OU2 : "OU",
-  OU3 : "OU",
-  CN : "CN"
-}
-*/
-
 var forge = require("node-forge");
 
 function domainDefault(data, privateKey, publicKey) {
   var csr = forge.pki.createCertificationRequest();
   csr.publicKey = publicKey;
   csr.setSubject([{
-      shortName: 'C',
-      value: data.C
-    }, {
-      shortName: 'O',
-      value: data.O
-    }, {
-      shortName: 'OU',
-      value: data.OU1
-    }, {
-      shortName: 'OU',
-      value: data.OU2
-    }, {
-      shortName: 'OU',
-      value: data.OU3
-    }, {
       shortName: 'CN',
       value: data.CN
     },{
       shortName: 'L',
       value: data.L
+    },{
+      shortName: 'O',
+      value: data.O
+    },{
+      shortName: 'C',
+      value: data.C
     }
   ]);
 
--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 var gen = require("./keypair/gen.js");
 var load = require("./keypair/load.js");
 var save = require("./keypair/save.js");
+var ServerPass_Standard = require("./csr/ServerPass_Standard.js");
 
-var d = require("./csr/domainDefault.js");
+exports.load = function(privateKey, password) {
+  return api(load(privateKey,password));
+};
 
+exports.gen = function(keylength) {
+  return gen(2048).then(function(keypair) {
+    return api(keypair);
+  });
+};
+
+
+function csr(keypair) {
+
+  var save = {
+    privateKey : function() {
+      return save.privateKey(keypair.privateKey);
+    },
+    publicKey : function() {
+      return save.publicKey(keypair.publicKey);
+    }
+  };
+
+  var csr = {
+    ServerPass_Standard : function(data) {
+      ServerPass_Standard(data, keypair.privateKey, keypair.publicKey);
+    }
+  };
+
+  return {
+    csr : csr,
+    save : save
+  };
+}
+
+
+/*
 gen(2048).then(function(keypair) {
 
-  /*
+
   var a = save.privateKey(privateKey);
   var b = save.privateKey(privateKey,"test");
   console.log("a", load(a));
   console.log("b", load(b));
   console.log("c", load(b,"aadsfs"));
-  console.log("d", load(b,"test"));*/
+  console.log("d", load(b,"test"));
 
-/*
   var a = save.privateKey(keypair.privateKey);
   console.log(a);
   var b = save.publicKey(keypair.publicKey);
   console.log(b);
-*/
 
-/*
   console.log(d({
-    C : "DE",
-    O : "O",
-    L : "link-edv.de",
-    OU1 : "OU",
-    OU2 : "OU",
-    OU3 : "OU",
-    CN : "link-edv.de"
+    CN : "*.link-edv.de",
+    L : "Herdorf",
+    O : "test",
+    C : "DE"
   }, keypair.privateKey, keypair.publicKey));
-*/
-});
+}); */
