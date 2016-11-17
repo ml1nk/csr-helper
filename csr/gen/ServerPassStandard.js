8e62914fda33de889f7304761b0d3ce7f117a9e7
Marcel Link <marcel.link@t-online.de>
2016-11-17 18:24:02 +0100

    ^ 0.0.13
    + more fields for ServerPassStandard

--- csr/gen/ServerPassStandard.js
+++ csr/gen/ServerPassStandard.js
@@ -3,7 +3,8 @@ var forge = require("../../forge.min.js");
 function domainDefault(data, privateKey, publicKey) {
   var csr = forge.pki.createCertificationRequest();
   csr.publicKey = publicKey;
-  csr.setSubject([{
+
+  var subject = [{
       shortName: 'CN',
       value: data.CN
     },{
@@ -16,7 +17,42 @@ function domainDefault(data, privateKey, publicKey) {
       shortName: 'C',
       value: data.C
     }
-  ]);
+  ];
+
+  for(let i=0; i<5; i++) {
+    if(data.hasOwnProperty("OU"+i)) {
+      subject.push({
+        shortName: 'OU',
+        value: data["OU"+i]
+      });
+    }
+  }
+
+  if(data.hasOwnProperty("ST")) {
+    subject.push({
+      shortName: 'ST',
+      value: data.ST
+    });
+  }
+
+  if(data.hasOwnProperty("streetAddress")) {
+    subject.push({
+      name: "streetAddress",
+      type: '2.5.4.9',
+      value: data.streetAddress
+    });
+  }
+
+  if(data.hasOwnProperty("postalCode")) {
+    subject.push({
+      name: "postalCode",
+      type: '2.5.4.17',
+      value: data.postalCode
+    });
+  }
+
+
+  csr.setSubject(subject);
   csr.sign(privateKey,forge.md.sha256.create());
   if(!csr.verify()) {
     return false;
